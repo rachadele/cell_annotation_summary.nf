a9f9fd9820f9a5496a79267e201c24e753bed7c6
rachadele <raschwaa@gmail.com>
2025-10-14 16:31:08 -0700

    delete old results

--- hsap_2024/19/c800ae27684afcba7a735d7eab7d47/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-python /space/grp/rschwartz/rschwartz/evaluation_summary.nf/bin/plot_contrasts.py --emmeans_estimates method_emmeans_estimates.tsv \
-                --emmeans_summary method_emmeans_summary.tsv \
-                --key family \
-                --weighted_f1_results weighted_f1_results.tsv
--- hsap_2024/84/e0f952aac668f17660397a61cbcf13/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-python /space/grp/rschwartz/rschwartz/evaluation_summary.nf/bin/plot_contrasts.py --emmeans_estimates disease_state_emmeans_estimates.tsv \
-                --emmeans_summary disease_state_emmeans_summary.tsv \
-                --key subclass \
-                --weighted_f1_results weighted_f1_results.tsv
